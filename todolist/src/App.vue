dc4638ea964a03e6d6a1d3ec1e5199bfa7fbe671
Taylor Jensen <Taylorlcj@gmail.com>
2023-03-06 00:21:44 -0500

    this form adds a todo

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -41,7 +41,32 @@ onMounted(() => {
 					placeholder="e.g. make a video"
 					v-model="input_content"
 				/>
-				{{ input_content }}
+				<h4>Pick a category</h4>
+
+				<div class="options">
+					<label>
+						<input
+							type="radio"
+							name="category"
+							value="business"
+							v-model="input_category"
+						/>
+						<span class="bubble business"></span>
+						<div>Business</div>
+					</label>
+					<label>
+						<input
+							type="radio"
+							name="category"
+							value="business"
+							v-model="input_category"
+						/>
+						<span class="bubble personal"></span>
+						<div>Personal</div>
+					</label>
+				</div>
+
+				<input type="submit" value="Add todo" />
 			</form>
 		</section>
 	</main>
